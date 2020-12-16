77ac554a106a8ae3492d9127667f11a0f4cb9dd2
yacooler <yacooler@yandex.ru>
2020-12-16 11:59:38 +0300

    Initial commit

---
+++ src/main/java/ru/vyazankin/springboot/sprongboothomework/SprongBootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.vyazankin.springboot.sprongboothomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprongBootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprongBootHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vyazankin/springboot/sprongboothomework/SprongBootHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vyazankin.springboot.sprongboothomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprongBootHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
